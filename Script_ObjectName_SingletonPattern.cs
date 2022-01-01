cf91838bab9facb2cf031e08d89db188ea7af789
Emil M <40560628+foobar-git@users.noreply.github.com>
2022-01-01 11:03:46 +0100

    Rename file

--- Script_ObjectName_SingletonPattern.cs
+++ Script_ObjectName_SingletonPattern.cs
@@ -1,10 +1,13 @@
+//====================================================================================================
+// USE THIS SCRIPT IF YOU WANT YOUR INSTANCE TO BE PUBLIC
+//====================================================================================================
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Script_ObjectName_SingletonPattern : MonoBehaviour
-{ // use this script if you want your instance to be public
-
+{
     private static Script_ObjectName_SingletonPattern instance;
 
     public Script_ObjectName_SingletonPattern GetInstance_ObjectName() {
